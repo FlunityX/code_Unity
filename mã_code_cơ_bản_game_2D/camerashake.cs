8c535d93853ba7a3dcedc6235d739174ab7bf043
nguyễn mạnh <manharia123@gmail.com>
2024-03-03 15:04:23 +0700

    cammera shake

---
+++ mã_code_cơ_bản_game_2D/camerashake.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class camerashake : MonoBehaviour
+{
+    public CinemachineVirtualCamera cinemachinevirtualCamera;
+    public float shaketimer;
+
+
+    public static camerashake instance { get; private set; }
+
+    private void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogWarning("Another instance of camerashake already exists. Destroying this one.");
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+        cinemachinevirtualCamera = GetComponent<CinemachineVirtualCamera>();
+    }
+
+    private void Start()
+    {
+        cinemachinevirtualCamera =GetComponent<CinemachineVirtualCamera>();
+    }
+
+
+    public void shakecamera(float intensity, float time)
+    {
+        CinemachineBasicMultiChannelPerlin cinemachineBasicMultiChannelPerlin =
+            cinemachinevirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = intensity;
+        shaketimer = time;
+    }
+
+    private void Update()
+    {
+
+            if (shaketimer > 0)
+            {
+                shaketimer -= Time.deltaTime;
+                if(shaketimer <=0)
+                {
+                    CinemachineBasicMultiChannelPerlin cinemachineBasicMultiChannelPerlin =
+                        cinemachinevirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+                    cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = 0f;
+                    shaketimer = 0;
+                }
+        }
+    }
+}
