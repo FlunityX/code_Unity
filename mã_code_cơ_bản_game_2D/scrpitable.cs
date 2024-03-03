018c8a07b23f627e6c3b24cd2fc11a551725ac77
nguyễn mạnh <manharia123@gmail.com>
2024-03-03 15:08:31 +0700

    scriptTableObject

---
+++ mã_code_cơ_bản_game_2D/scrpitable.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "score", menuName = "score/highscore")]
+
+public class scrpitable : ScriptableObject
+{
+    // Start is called before the first frame update
+    public int highscore=0;
+}
